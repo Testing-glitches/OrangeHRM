0c2dbae5781edfcca68ef292c8042de058caa4b0
Testing-glithes <jitcse353@gmail.com>
2026-05-21 15:47:44 +0530

    push Auth.setup.spec.js

--- tests/Setup/Auth.setup.spec.js
+++ tests/Setup/Auth.setup.spec.js
@@ -1,9 +1,12 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('Login and save auth session', async ({ page }) => {
 
-  // Open Login Page
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  // Increase timeout because website can be slow
+  test.setTimeout(60000);
+
+  // Open OrangeHRM Login Page
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',{waitUntil: 'domcontentloaded'});
 
   // Enter Username
   await page.getByRole('textbox', {name: 'Username'}).fill('Admin');
@@ -11,13 +14,13 @@ test('Login and save auth session', async ({ page }) => {
   // Enter Password
   await page.getByRole('textbox', {name: 'Password'}).fill('admin123');
 
-  // Click Login
+  // Click Login Button
   await page.getByRole('button', {name: 'Login'}).click();
 
-  // Verify Dashboard URL
+  // Verify Dashboard URL after successful login
   await page.waitForURL(/dashboard/);
 
-  // Save Login Session
+  // Save Login Session in auth.json file
   await page.context().storageState({path: 'auth.json'});
 
   console.log('Auth session saved successfully');
